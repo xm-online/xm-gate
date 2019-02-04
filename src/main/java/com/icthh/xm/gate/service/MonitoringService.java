566314935425827f69f63cdf1c7a5494a48aa5c2
vkirichenko <vitaliy.v.kirichenko@gmail.com>
2019-02-04 16:37:14 +0200

    refactoring

--- src/main/java/com/icthh/xm/gate/service/MonitoringService.java
+++ src/main/java/com/icthh/xm/gate/service/MonitoringService.java
@@ -23,7 +23,7 @@ public class MonitoringService {
         MetricsClient client = Feign.builder().target(Target.EmptyTarget.create(MetricsClient.class));
 
         String s = client.get(URI.create("http://localhost:8080"),
-            "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJjcmVhdGVUb2tlblRpbWUiOjE1NDkyOTA1MzA5NTcsInVzZXJfbmFtZSI6ImNvbXBhc3MiLCJzY29wZSI6WyJvcGVuaWQiXSwicm9sZV9rZXkiOiJTVVBFUi1BRE1JTiIsInVzZXJfa2V5IjoiY29tcGFzcyIsImV4cCI6MTU0OTMzMzczMCwiYWRkaXRpb25hbERldGFpbHMiOnt9LCJsb2dpbnMiOlt7InR5cGVLZXkiOiJMT0dJTi5FTUFJTCIsInN0YXRlS2V5IjpudWxsLCJsb2dpbiI6ImNvbXBhc3NAeG0tb25saW5lLmNvbS51YSJ9LHsidHlwZUtleSI6IkxPR0lOLk5JQ0tOQU1FIiwic3RhdGVLZXkiOm51bGwsImxvZ2luIjoiY29tcGFzcyJ9XSwiYXV0aG9yaXRpZXMiOlsiU1VQRVItQURNSU4iXSwianRpIjoiMjg1YmJlOTUtYjBhOS00YzRjLThiMjAtYTczNWU4YjVlYjg1IiwidGVuYW50IjoiQ09NUEFTUyIsImNsaWVudF9pZCI6ImludGVybmFsIn0.kFpu6R-yM9Z4aQqmSdr38eH0StY_7qeGdcLLrm3u7ZMlj9tGdhAg_6ZeuBjG3IOrUpEKZHHXAo9qFVb2MvMF0I_LiJPcjTj9g4rnN_N5ZL1BemxyFlETWsiyqDKy2EjLxJbvuqV2nmfDebnpZ7PxlIZgpTUxvlFD6p0nnG80plaabYDgz-Iyrd_gWNWuBzIEF_cuyyunZ21r0tC0fy_VPXA1J0MK-GDRpvxbd9i6tLznYBV3KPpR_TSMWmPESeYenSPP4UU2rhtJSPAPttA9HuejAfmkiv9jPnSG5Hnmt4oLXenwj6g6y4H8hle4uqir4yliWe9uBjm93xUIsxioOA");
+            "");
         log.info(s);
         return Collections.emptyList();
     }
