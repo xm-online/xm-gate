057f18a68b6924b57122bd4256d01d95eed54e4d
vkirichenko <vitaliy.v.kirichenko@gmail.com>
2019-02-04 12:55:43 +0200

    refactoring

--- src/main/java/com/icthh/xm/gate/web/rest/dto/ServiceHealth.java
+++ src/main/java/com/icthh/xm/gate/web/rest/dto/ServiceHealth.java
@@ -2,12 +2,12 @@ package com.icthh.xm.gate.web.rest.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.boot.actuate.health.Health;
 
 @Data
 @Builder
 public class ServiceHealth {
 
     private String instanceId;
-    private String rawOutput;
-    private String status;
+    private Health health;
 }
--- src/main/java/com/icthh/xm/gate/web/rest/dto/ServiceMetrics.java
+++ src/main/java/com/icthh/xm/gate/web/rest/dto/ServiceMetrics.java
@@ -2,13 +2,12 @@ package com.icthh.xm.gate.web.rest.dto;
 
 import lombok.Builder;
 import lombok.Data;
-
-import java.util.Map;
+import org.springframework.boot.actuate.metrics.MetricsEndpoint;
 
 @Data
 @Builder
 public class ServiceMetrics {
 
     private String instanceId;
-    private Map<String, Object> value;
+    private MetricsEndpoint.MetricResponse metrics;
 }
