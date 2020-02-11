9269636024fbf49bea8f530255d87b4812d24912
Yuriy Kruchko <yuriy.kruchko@xm-online.com>
2020-02-11 16:43:36 +0200

    remove unneeded file

--- 1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-PROJECT_VERSION=`./gradlew -q  getProjectVersion`
-tags="$PROJECT_VERSION $(echo $PROJECT_VERSION | awk -F '.' '{printf $1"."$2" "$1}')"
-for t in $tags 
-do
-    echo "$t"
-done
-
