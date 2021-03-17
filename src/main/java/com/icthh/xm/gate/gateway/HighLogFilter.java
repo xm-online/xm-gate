baacdf95f9444c6e8aff21bee229a8dc0e248e42
Anatoliy Medvedchuk <anatoliy.medvedchuk@jevera.software>
2021-03-17 11:37:13 +0200

    Implement logging with PRE filter if chain break before LoggingFilter

--- src/main/java/com/icthh/xm/gate/gateway/HighLogFilter.java
+++ src/main/java/com/icthh/xm/gate/gateway/HighLogFilter.java
@@ -13,6 +13,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Filter for logging all HTTP requests with highest precedence
@@ -26,22 +27,37 @@ public class HighLogFilter implements Filter {
     public void doFilter(final ServletRequest request, final ServletResponse response,
                          final FilterChain chain) throws IOException, ServletException {
 
+        String ridValue = "";
+        String domain = null;
+        String method = null;
+        String requestUri = null;
+
         try {
             if (request instanceof HttpServletRequest) {
                 HttpServletRequest req = (HttpServletRequest) request;
 
-                String domain = request.getServerName();
-                String method = req.getMethod();
-                String requestUri = req.getRequestURI();
-                MdcUtils.putRid(MdcUtils.generateRid());
+                domain = request.getServerName();
+                method = req.getMethod();
+                requestUri = req.getRequestURI();
+                ridValue = MdcUtils.generateRid();
+                MdcUtils.putRid(ridValue);
 
-                log.info("PRE FILTER {} --> {} {}", domain, method, requestUri);
+                log.info("PRE FILTER: START: {} --> {} {}", domain, method, requestUri);
             }
         } catch (Exception e) {
             log.warn("Error during RiD processing: {}", e.getMessage(), e);
         }
 
         chain.doFilter(request, response);
+
+        // if Rid was not modified by standard LoggingFilter we need to log the http status
+        if (ridValue.equals(MdcUtils.getRid())) {
+            Integer status = null;
+            if (response instanceof HttpServletResponse) {
+                status = ((HttpServletResponse) response).getStatus();
+            }
+            log.info("PRE FILTER:  STOP: {} --> {} {} {}", domain, status, method, requestUri);
+        }
     }
 
 }
