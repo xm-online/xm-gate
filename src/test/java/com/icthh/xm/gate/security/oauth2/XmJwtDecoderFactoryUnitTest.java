5891e301fb409eee9b2b3fb65b819d3afdba9d3b
olye0216 <oleksandr.yershov@netcracker.com>
2021-03-18 21:20:26 +0200

    refactor: increase xm-commons version, unit test fix

--- src/test/java/com/icthh/xm/gate/security/oauth2/XmJwtDecoderFactoryUnitTest.java
+++ src/test/java/com/icthh/xm/gate/security/oauth2/XmJwtDecoderFactoryUnitTest.java
@@ -43,7 +43,7 @@ public class XmJwtDecoderFactoryUnitTest {
         assertNotNull(createdJwtDecoder);
         Map<String, JwtDecoder> cachedJwtDecoders = XmJwtDecoderFactory.getJwtDecoders(tenantKey);
         assertNotNull(cachedJwtDecoders);
-        JwtDecoder cachedJwtDecoder = cachedJwtDecoders.get(clientRegistration);
+        JwtDecoder cachedJwtDecoder = cachedJwtDecoders.get(registrationId);
         assertNotNull(cachedJwtDecoder);
     }
 
