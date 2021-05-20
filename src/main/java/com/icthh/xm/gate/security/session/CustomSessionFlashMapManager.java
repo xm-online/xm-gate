08d36ae2ee054bfc48208d4f58ea3bce31bd5818
olye0216 <oleksandr.yershov@netcracker.com>
2021-05-20 22:14:14 +0300

    #hz8fqz refactor

--- src/main/java/com/icthh/xm/gate/security/session/CustomSessionFlashMapManager.java
+++ src/main/java/com/icthh/xm/gate/security/session/CustomSessionFlashMapManager.java
@@ -9,11 +9,7 @@ import org.springframework.web.servlet.support.SessionFlashMapManager;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
-import java.util.TimeZone;
 
 @Slf4j
 public class CustomSessionFlashMapManager extends SessionFlashMapManager {
@@ -37,24 +33,15 @@ public class CustomSessionFlashMapManager extends SessionFlashMapManager {
 
             if (session != null) {
                 log.warn("Session with JSESSIONID '{}' invalid.", session.getId());
-                log.warn("Session '{}' created at '{}'", session.getId(), convertToDate(session.getCreationTime()));
-                log.warn("Session '{}' last accessed at '{}'", session.getId(), convertToDate(session.getLastAccessedTime()));
                 List.of(servletRequest.getCookies()).forEach(cookie -> {
                     log.warn("Session cookie name '{}', value '{}', maxage '{}'",
                         cookie.getName(), cookie.getValue(), cookie.getMaxAge());
                 });
             }
+        } finally {
             log.warn("Clearing cookies and perform logout.");
             servletRequest.logout();
         }
         return null;
-    }
-
-    private String convertToDate(long timeStamp) {
-        Date date = new Date(timeStamp);
-        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-        formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
-
-        return formatter.format(date);
     }
 }
