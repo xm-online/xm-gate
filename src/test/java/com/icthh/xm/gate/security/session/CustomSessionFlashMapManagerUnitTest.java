a8bb2dd1a55b43263e205eba587fc3252b609002
olye0216 <oleksandr.yershov@netcracker.com>
2021-07-15 16:18:18 +0300

    #hz8fqz CustomSessionFlashMapManager: unit test improve

--- src/test/java/com/icthh/xm/gate/security/session/CustomSessionFlashMapManagerUnitTest.java
+++ src/test/java/com/icthh/xm/gate/security/session/CustomSessionFlashMapManagerUnitTest.java
@@ -1,5 +1,6 @@
 package com.icthh.xm.gate.security.session;
 
+import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,11 +9,14 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.web.servlet.FlashMap;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
+import static com.icthh.xm.gate.config.Constants.JSESSIONID_COOKIE_NAME;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -29,10 +33,18 @@ public class CustomSessionFlashMapManagerUnitTest {
 
     @Test
     public void test_shouldHandleException() {
+        MockHttpSession mockHttpSession = new MockHttpSession();
+        Cookie[] cookies = Arrays.array(new Cookie(JSESSIONID_COOKIE_NAME, "f95808d8-254a-40c7-9561-b1b4fe001307"));
+
         when(httpServletRequest.getSession(false))
             .thenThrow(new IllegalStateException())
-            .thenReturn(null);
+            .thenReturn(mockHttpSession);
+
+        when(httpServletRequest.getCookies())
+            .thenReturn(cookies);
+
         List<FlashMap> flashMaps = customSessionFlashMapManager.retrieveFlashMaps(httpServletRequest);
+
         Assertions.assertNull(flashMaps);
     }
 }
