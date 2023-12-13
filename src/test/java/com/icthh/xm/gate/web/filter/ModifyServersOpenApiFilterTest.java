3ade21ca724f9ed0b1e59dc45ac5058279049888
osiievska23 <valentinaosievskaya@gmail.com>
2023-12-13 19:18:57 +0100

    MID-996 spring security 6.2

--- src/test/java/com/icthh/xm/gate/web/filter/ModifyServersOpenApiFilterTest.java
+++ src/test/java/com/icthh/xm/gate/web/filter/ModifyServersOpenApiFilterTest.java
@@ -44,7 +44,7 @@ class ModifyServersOpenApiFilterTest {
         ServerWebExchange exchange = MockServerWebExchange.from(request);
 
         // apply the filter to the request
-        ModifyServersOpenApiFilter modifyServersOpenApiFilter = spy(new ModifyServersOpenApiFilter(routeLocator));
+        ModifyServersOpenApiFilter modifyServersOpenApiFilter = spy(new ModifyServersOpenApiFilter());
         modifyServersOpenApiFilter.filter(exchange, filterChain).subscribe();
 
         verify(modifyServersOpenApiFilter, times(1))
@@ -61,7 +61,7 @@ class ModifyServersOpenApiFilterTest {
         ServerWebExchange exchange = MockServerWebExchange.from(request);
 
         // apply the filter to the request
-        ModifyServersOpenApiFilter modifyServersOpenApiFilter = spy(new ModifyServersOpenApiFilter(routeLocator));
+        ModifyServersOpenApiFilter modifyServersOpenApiFilter = spy(new ModifyServersOpenApiFilter());
         modifyServersOpenApiFilter.filter(exchange, filterChain).subscribe();
 
         verify(modifyServersOpenApiFilter, times(0))
@@ -70,7 +70,7 @@ class ModifyServersOpenApiFilterTest {
 
     @Test
     void shouldOrderToMinusOne() {
-        ModifyServersOpenApiFilter modifyServersOpenApiFilter = new ModifyServersOpenApiFilter(routeLocator);
+        ModifyServersOpenApiFilter modifyServersOpenApiFilter = new ModifyServersOpenApiFilter();
         assertEquals(modifyServersOpenApiFilter.getOrder(), -1);
     }
 
@@ -80,7 +80,7 @@ class ModifyServersOpenApiFilterTest {
         private final String path = "/services/service-test/instance-test/v3/api-docs";
         private final MockServerHttpRequest request = MockServerHttpRequest.get(path).build();
         private final ServerWebExchange exchange = MockServerWebExchange.from(request);
-        private final ModifyServersOpenApiFilter modifyServersOpenApiFilter = new ModifyServersOpenApiFilter(routeLocator);
+        private final ModifyServersOpenApiFilter modifyServersOpenApiFilter = new ModifyServersOpenApiFilter();
 
         @Test
         void shouldRewriteBodyWhenBodyIsFluxAndResponseIsNotZipped() {
