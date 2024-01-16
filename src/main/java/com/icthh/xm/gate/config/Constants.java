cee56db9384c042516fdeba668e2b49d150c8d27
osiievska23 <valentinaosievskaya@gmail.com>
2024-01-16 16:31:09 +0100

    added route locator enhancer to enrich discovered routes with rate limiting filter

--- src/main/java/com/icthh/xm/gate/config/Constants.java
+++ src/main/java/com/icthh/xm/gate/config/Constants.java
@@ -11,6 +11,7 @@ public final class Constants {
     public static final String HEADER_WEBAPP_URL = "x-webapp-url";
 
     public static final int FILTER_ORDER_TENANT_INIT = Ordered.HIGHEST_PRECEDENCE;
+    public static final int FILTER_DOMAIN_RELAY_ORDER = Ordered.HIGHEST_PRECEDENCE + 1;
     public static final String DEFAULT_TENANT = "XM";
 
     private Constants() {}
