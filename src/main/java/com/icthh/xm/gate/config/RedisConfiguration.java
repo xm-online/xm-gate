c0a1645d1d5418655163412c75e968a5a01fcd51
ichupryna <ichupryna@gmail.com>
2024-02-15 16:15:49 +0200

    Adding redis configuration

---
+++ src/main/java/com/icthh/xm/gate/config/RedisConfiguration.java
@@ -0,0 +1,40 @@
+package com.icthh.xm.gate.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+import java.time.Duration;
+
+@Configuration
+@Slf4j
+public class RedisConfiguration {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private String port;
+
+    @Value("${spring.data.redis.database}")
+    private int database;
+
+    @Bean
+    protected LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(host);
+        config.setPort(Integer.parseInt(port));
+        config.setDatabase(database);
+        log.info("LettuceConnectionFactory: host=" + config.getHostName() + ":" + redisConnectionFactory().getPort() + " db=" + config.getDatabase());
+        LettuceClientConfiguration lettuceClientConfiguration = LettuceClientConfiguration.builder()
+            .clientName("gate")
+            .commandTimeout(Duration.ofMillis(5000))
+            .build();
+        return new LettuceConnectionFactory(config, lettuceClientConfiguration);
+    }
+
+}
