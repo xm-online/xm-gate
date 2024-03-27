650f90dd28e857a0e9206eb147908fe0c513bc35
Gitlab CI/CD <gitlab@jevera.software>
2024-03-27 09:54:32 +0200

    Clean up code

--- src/main/java/com/icthh/xm/gate/utils/oauth2/CookieUtils.java
+++ src/main/java/com/icthh/xm/gate/utils/oauth2/CookieUtils.java
@@ -5,21 +5,20 @@ import org.springframework.util.SerializationUtils;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
 import java.util.Base64;
+import java.util.Objects;
 import java.util.Optional;
 
 public class CookieUtils {
 
     public static Optional<Cookie> getCookie(HttpServletRequest request, String name) {
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals(name)) {
-                    return Optional.of(cookie);
-                }
-            }
-        }
-        return Optional.empty();
+        return Optional.ofNullable(request.getCookies())
+            .stream()
+            .flatMap(Arrays::stream)
+            .filter(Objects::nonNull)
+            .filter(cookie -> name.equals(cookie.getName()))
+            .findFirst();
     }
 
     public static void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
