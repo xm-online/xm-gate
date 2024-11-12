1759cdae9a030365dfd24920114fa43bbccc21f6
ichupryna <ichupryna@gmail.com>
2024-11-12 19:21:23 +0200

    Adding missing changes

--- src/main/java/com/icthh/xm/gate/config/Config.java
+++ src/main/java/com/icthh/xm/gate/config/Config.java
@@ -2,6 +2,7 @@ package com.icthh.xm.gate.config;
 
 import io.micrometer.observation.ObservationRegistry;
 import io.micrometer.observation.aop.ObservedAspect;
+import io.micrometer.tracing.Tracer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,4 +13,10 @@ public class Config {
     ObservedAspect observedAspect(ObservationRegistry observationRegistry) {
         return new ObservedAspect(observationRegistry);
     }
+
+    @Bean
+    public Tracer tracer() {
+        return Tracer.NOOP;
+    }
+
 }
