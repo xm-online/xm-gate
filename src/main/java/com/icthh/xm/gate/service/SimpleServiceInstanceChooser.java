b55ea153f62ea025993f003311e06c073b0e5bb4
ssenko <serhii.senko@jevera.software>
2025-04-29 09:39:00 +0300

    add condition to service chooser

--- src/main/java/com/icthh/xm/gate/service/SimpleServiceInstanceChooser.java
+++ src/main/java/com/icthh/xm/gate/service/SimpleServiceInstanceChooser.java
@@ -6,11 +6,13 @@ import org.springframework.cloud.client.loadbalancer.ServiceInstanceChooser;
 import org.springframework.cloud.netflix.ribbon.RibbonLoadBalancerClient;
 import org.springframework.cloud.netflix.ribbon.support.AbstractLoadBalancingClient;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
 @RequiredArgsConstructor
+@Profile("noconsul")
+@Primary
 public class SimpleServiceInstanceChooser implements ServiceInstanceChooser {
 
     private final RibbonLoadBalancerClient delegate;
