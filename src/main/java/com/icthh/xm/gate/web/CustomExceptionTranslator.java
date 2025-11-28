6337200b8e9931d49bafa28b0918fa2d44f8ecb0
osiievska23 <valentinaosievskaya@gmail.com>
2025-11-28 10:58:05 +0200

    added CustomExceptionTranslator

---
+++ src/main/java/com/icthh/xm/gate/web/CustomExceptionTranslator.java
@@ -0,0 +1,37 @@
+package com.icthh.xm.gate.web;
+
+import com.icthh.xm.commons.exceptions.EntityNotFoundException;
+import com.icthh.xm.commons.exceptions.ErrorConstants;
+import com.icthh.xm.commons.i18n.error.domain.vm.ErrorVM;
+import com.icthh.xm.commons.i18n.error.web.ExceptionTranslator;
+import com.icthh.xm.commons.i18n.spring.service.LocalizationMessageService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/*
+    Move these handlers to commons after migrating to new commons version
+ */
+@Slf4j
+@ControllerAdvice
+public class CustomExceptionTranslator extends ExceptionTranslator {
+
+    private final LocalizationMessageService localizationMessageService;
+
+    public CustomExceptionTranslator(LocalizationMessageService localizationErrorMessageService) {
+        super(localizationErrorMessageService);
+        this.localizationMessageService = localizationErrorMessageService;
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
+    public ErrorVM processNotFoundError(EntityNotFoundException ex) {
+        log.debug("Entity not found", ex);
+        return new ErrorVM(ErrorConstants.ERR_NOTFOUND,
+            localizationMessageService.getMessage(ErrorConstants.ERR_NOTFOUND));
+    }
+}
