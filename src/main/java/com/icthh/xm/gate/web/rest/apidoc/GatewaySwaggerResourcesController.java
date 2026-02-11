43f1ac436bd89458c0ae1aeaff260f6fa9a2c460
osiievska23 <valentinaosievskaya@gmail.com>
2026-02-11 17:18:54 +0200

    fix swagger resources api

--- src/main/java/com/icthh/xm/gate/web/rest/apidoc/GatewaySwaggerResourcesController.java
+++ src/main/java/com/icthh/xm/gate/web/rest/apidoc/GatewaySwaggerResourcesController.java
@@ -3,9 +3,9 @@ package com.icthh.xm.gate.web.rest.apidoc;
 import com.icthh.xm.gate.config.properties.ApplicationProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections.CollectionUtils;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.consul.discovery.ConsulServiceInstance;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+
+import static org.apache.commons.collections.CollectionUtils.isNotEmpty;
 
 /**
  * Retrieves all registered microservices Swagger resources.
@@ -29,7 +31,7 @@ public class GatewaySwaggerResourcesController {
     private static final String SWAGGER_VERSION_3 = "3.0";
     public static final String API_DOCS_PATH_V2 = "v2/api-docs";
     public static final String API_DOCS_PATH_V3 = "v3/api-docs";
-    private static final String SWAGGER_V3 = "v3";
+    private static final String SWAGGER_V2_TAG = "swagger=v2";
 
     private final DiscoveryClient discoveryClient;
     private final ApplicationProperties applicationProperties;
@@ -39,29 +41,29 @@ public class GatewaySwaggerResourcesController {
         List<SwaggerResource> resources = new ArrayList<>();
         Set<String> excludedServices = applicationProperties.getGateway().getExcludedServices();
 
-        // Add the default swagger resource that corresponds to the gateway's own swagger doc
-        resources.add(createSwaggerResource("default", API_DOCS_PATH_V2, SWAGGER_VERSION_2));
+        resources.add(createSwaggerResource("default", API_DOCS_PATH_V3, SWAGGER_VERSION_3));
 
-        // Add the registered microservices swagger docs as additional swagger resources
         discoveryClient.getServices().stream()
             .filter(serviceId -> !excludedServices.contains(serviceId))
-            .forEach(serviceId -> {
-                List<ServiceInstance> instances = discoveryClient.getInstances(serviceId);
-                log.debug("Service {} instances.size={}", serviceId, instances.size());
-
-                String swaggerVersion = SWAGGER_VERSION_2;
-                String apiDocsPath = API_DOCS_PATH_V2;
-
-                if (CollectionUtils.isNotEmpty(instances)) {
-                    if (SWAGGER_V3.equalsIgnoreCase(instances.get(0).getMetadata().get("swagger"))) {
-                        swaggerVersion = SWAGGER_VERSION_3;
-                        apiDocsPath = API_DOCS_PATH_V3;
-                    }
-                }
-                resources.add(createSwaggerResource(serviceId, "/" + serviceId + "/" + apiDocsPath, swaggerVersion));
-            });
+            .map(this::toSwaggerResource)
+            .forEach(resources::add);
 
         return resources;
+    }
+
+    private SwaggerResource toSwaggerResource(String serviceId) {
+        List<ServiceInstance> instances = discoveryClient.getInstances(serviceId);
+        log.debug("Service {} instances.size={}", serviceId, instances.size());
+
+        boolean isV2 = serviceHasSwaggerV2Tag(instances);
+        String swaggerVersion = isV2 ? SWAGGER_VERSION_2 : SWAGGER_VERSION_3;
+        String apiDocsPath = isV2 ? API_DOCS_PATH_V2 : API_DOCS_PATH_V3;
+
+        return createSwaggerResource(serviceId, "/" + serviceId + "/" + apiDocsPath, swaggerVersion);
+    }
+
+    private boolean serviceHasSwaggerV2Tag(List<ServiceInstance> instances) {
+        return isNotEmpty(instances) && ((ConsulServiceInstance) instances.getFirst()).getTags().contains(SWAGGER_V2_TAG);
     }
 
     private SwaggerResource createSwaggerResource(String name, String location, String swaggerVersion) {
