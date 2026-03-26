f60f997d16cd8a626872a33ed23a503f51d33d27
osiievska23 <valentinaosievskaya@gmail.com>
2026-03-26 11:50:35 +0200

    local resolver normalization

--- src/main/java/com/icthh/xm/gate/config/LocaleConfiguration.java
+++ src/main/java/com/icthh/xm/gate/config/LocaleConfiguration.java
@@ -1,14 +1,23 @@
 package com.icthh.xm.gate.config;
 
 import com.icthh.xm.commons.i18n.spring.config.CommonMessageSourceConfiguration;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.i18n.LocaleContext;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.util.WebUtils;
+
+import java.util.Locale;
+
+import static org.apache.logging.log4j.util.Strings.EMPTY;
+import static org.springframework.util.StringUtils.replace;
 
 @Configuration
 @Import({CommonMessageSourceConfiguration.class})
@@ -16,7 +25,7 @@ public class LocaleConfiguration implements WebMvcConfigurer {
 
     @Bean(name = "localeResolver")
     public LocaleResolver localeResolver() {
-        return new CookieLocaleResolver("NG_TRANSLATE_LANG_KEY");
+        return new CustomCookieLocaleResolver();
     }
 
     @Override
@@ -24,5 +33,38 @@ public class LocaleConfiguration implements WebMvcConfigurer {
         LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
         localeChangeInterceptor.setParamName("language");
         registry.addInterceptor(localeChangeInterceptor);
+    }
+
+    private static class CustomCookieLocaleResolver extends CookieLocaleResolver {
+
+        private static final String QUOTE = "%22";
+        private static final String COOKIE_NAME = "NG_TRANSLATE_LANG_KEY";
+
+        public CustomCookieLocaleResolver() {
+            super(COOKIE_NAME);
+        }
+
+        @Override
+        public LocaleContext resolveLocaleContext(HttpServletRequest request) {
+            HttpServletRequest normalizedRequest = normalizeLocaleCookie(request);
+            return super.resolveLocaleContext(normalizedRequest);
+        }
+
+        @Override
+        public Locale resolveLocale(HttpServletRequest request) {
+            HttpServletRequest normalizedRequest = normalizeLocaleCookie(request);
+            return super.resolveLocale(normalizedRequest);
+        }
+
+        private HttpServletRequest normalizeLocaleCookie(HttpServletRequest request) {
+            Cookie cookie = WebUtils.getCookie(request, COOKIE_NAME);
+            if (cookie != null) {
+                String lang = cookie.getValue();
+                lang = replace(lang, QUOTE, EMPTY);
+                lang = replace(lang, "\"", EMPTY);
+                cookie.setValue(lang);
+            }
+            return request;
+        }
     }
 }
