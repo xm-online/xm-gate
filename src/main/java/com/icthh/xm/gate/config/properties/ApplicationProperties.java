f4a620210c1654c3c8dc4d09bea81f2bff68143a
Bohdan Volokhonenko <null>
2026-07-23 13:51:54 +0300

    extended properties

--- src/main/java/com/icthh/xm/gate/config/properties/ApplicationProperties.java
+++ src/main/java/com/icthh/xm/gate/config/properties/ApplicationProperties.java
@@ -104,5 +104,9 @@ public class ApplicationProperties {
         private Integer maxConnections;
         private Integer maxConnectionsPerRoute;
         private Integer connectionTimeoutSeconds;
+        private Integer socketTimeoutSeconds;
+        private Integer responseTimeoutSeconds;
+        private Integer connectionTtlSeconds;
+        private Integer evictIdleSeconds;
     }
 }
